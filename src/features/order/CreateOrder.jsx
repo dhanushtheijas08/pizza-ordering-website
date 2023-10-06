ab54bcd0b4491971217a5248f233441c3583ba9b
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-10-06 11:57:17 +0530

    user can get address by lat and lng

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,38 +1,15 @@
-import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-
-// https://uibakery.io/regex-library/phone-number
+import { useDispatch, useSelector } from 'react-redux';
+import store from '../../store';
+import { clearItem } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import { updateAddress } from '../user/userSlice';
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
   const navigation = useNavigation();
@@ -40,13 +17,13 @@ function CreateOrder() {
   const formErrors = useActionData();
   const username = useSelector((state) => state.user.username);
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
-
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.carts);
+  if (cart.length < 1) return <EmptyCart />;
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
-      {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -79,6 +56,7 @@ function CreateOrder() {
               type="text"
               name="address"
               required
+              onClick={() => dispatch(updateAddress)}
             />
           </div>
         </div>
@@ -95,6 +73,7 @@ function CreateOrder() {
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
           </label>
+          pizzaId
         </div>
 
         <div>
@@ -117,7 +96,11 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === 'on',
   };
-
+  order.cart.forEach((item) => {
+    item.pizzaId = item.id;
+    delete item.id;
+    item.totalPrice = item.quantity * item.unitPrice;
+  });
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
@@ -125,13 +108,9 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  // If everything is okay, create new order and redirect
-
-  // const newOrder = await createOrder(order);
-
-  // return redirect(`/order/${newOrder.id}`);
-
-  return null;
+  const newOrder = await createOrder(order);
+  store.dispatch(clearItem());
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   username: '',
+  userAddress: '',
 };
 
 const userSlice = createSlice({
@@ -11,9 +12,43 @@ const userSlice = createSlice({
     updateUserName(state, action) {
       state.username = action.payload;
     },
+    updateUserAddress(state, action) {
+      state.userAddress = action.payload;
+    },
   },
 });
 
-export const { updateUserName } = userSlice.actions;
+function getPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+async function getAddress({ lat, lng }) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+  );
+  const data = await res.json();
+  return data;
+}
+
+async function getuserAddress() {
+  const positionObj = await getPosition();
+  const position = {
+    lat: positionObj.coords.latitude,
+    lng: positionObj.coords.longitude,
+  };
+
+  const address = await getAddress(position);
+
+  return `${address.city}, ${address.countryName}`;
+}
+
+export const { updateUserName, updateUserAddress } = userSlice.actions;
+
+function updateAddress() {
+  return updateUserAddress({ type: 'updateUserAddress', payload: 'Hi' }); // Dispatch the correct action creator
+}
+export { updateAddress };
 
 export default userSlice.reducer;
